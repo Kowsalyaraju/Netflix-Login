bd3eb5062d44f456d7bbef06585cb0837557a50e
Kowsalyaraju <kowsalyaraju123@gmail.com>
2026-05-23 22:34:38 +0530

    Connected deployed backend

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const cors = require("cors")
 var defemail = "admin123@gmail.com"
 var defpass = "1234"
 
+const PORT = process.env.PORT || 5000
+
 app.use(cors())
 
 app.use(express.json())
@@ -24,6 +26,6 @@ app.post("/login",function(req,res)
     }
 })
 
-app.listen(5000,()=>{
-    console.log("Server is Running in Port 5000......")
+app.listen(PORT,()=>{
+   console.log(`Server Running on Port ${PORT}`)
 })
